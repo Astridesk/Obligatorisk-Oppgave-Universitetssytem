998766ee1ef00046e2cc42d83361e05df2b2f303
Astridesk <astridesk@uia.no>
2026-03-04 15:05:30 +0100

    legg til bruker

---
+++ Obligatorisk Oppgave 1/Bruker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obligatorisk_Oppgave_1
+{
+    public class Bruker     //hvorfor ikke internal?
+    {
+        //instansvariabler
+        private string Navn { get; set; } = string.Empty;
+        private string Epost {  get; set; } = string.Empty;
+
+        //konstruktør med paraametere
+        public Bruker(string navn, string epost)
+        {
+            Navn = navn;
+            Epost = epost;
+        }
+
+        public void SkrivUtInfo()
+        {
+            Console.WriteLine($"Navn: {Navn} \nEpost: { Epost}"); //string interpolation
+            
+        }
+
+    }
+}
--- Obligatorisk Oppgave 1/Program.cs
+++ Obligatorisk Oppgave 1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Serialization;
+
 Console.WriteLine("Hello, World!");
+
+public class Bruker;
+
+
+public class Student : Bruker;
